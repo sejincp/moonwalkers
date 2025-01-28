795c0d9a5a365458400a5d3c993189b4b1fa05f3
sejincp <sagela127@gmail.com>
2025-01-28 02:36:07 -0800

    NewMoonwalkPage - form revised

--- frontend/src/pages/NewMoonwalkPage/NewMoonwalkPage.jsx
+++ frontend/src/pages/NewMoonwalkPage/NewMoonwalkPage.jsx
@@ -1,17 +1,39 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import * as postService from '../../services/moonwalkService';
+import * as moonwalkService from '../../services/moonwalkService';
 
-export default function NewPostPage() {
-  const [content, setContent] = useState('');
+export default function NewMoonwalkPage() {
+  const [content, setContent] = useState({
+    date: new Date().toLocaleDateString().slice(10,0),
+    steps: 0,
+    comment: ''
+  });
+
+  const [miles, setMiles] = useState(0);
 
   const navigate = useNavigate();
+  
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    if ( name === 'steps' ) {
+      const steps = Number(value);
+      if (!isNaN(steps)) {
+        setMiles((steps / 2000).toFixed(2));
+      } else {
+        setMiles(0);
+      }
+    };
+    setContent({
+      ...content,
+      [name]: value
+    });
+  }
 
   async function handleSubmit(evt) {
     evt.preventDefault();
     try {
-      const post = await postService.create(content);
-      navigate('/posts');
+      const post = await moonwalkService.create(content);
+      navigate('/moonwalks');
     } catch (err) {
       console.log(err);
     }
@@ -21,25 +43,25 @@ export default function NewPostPage() {
     <>
       <h2>Add New Moonwalk</h2>
       <form autoComplete="off" onSubmit={handleSubmit}>
-      <label>Total Steps</label>
-        <input
-          type="text"
-          value={content}
-          onChange={(evt) => setContent(evt.target.value)}
-          required
-        />
       <label>Date</label>
         <input
-          type="text"
-          value={content}
-          onChange={(evt) => setContent(evt.target.value)}
+          type="date"
+          value={content.date}
+          onChange={handleChange}
+          required
+        />
+      <label>Total Steps</label>
+        <input
+          type="number"
+          value={content.steps}
+          onChange={handleChange}
           required
         />
         <label>Comment</label>
         <input
           type="text"
-          value={content}
-          onChange={(evt) => setContent(evt.target.value)}
+          value={content.comment}
+          onChange={handleChange}
           required
         />
         <button type="submit">Add Moonwalk</button>
